ce31d6ac49e3c1ac4d48f619e5c41c00366d234d
Bulat Farrakhov <bulat.farrakhov@skuvault.com>
2019-10-30 15:08:22 +0300

    GUARD-272: implemented orders service;

---
+++ src/SellbriteAccess/Exceptions/SellbriteNetworkException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace SellbriteAccess.Exceptions
+{
+	public class SellbriteNetworkException : SellbriteException
+	{
+		public SellbriteNetworkException( string message, Exception exception ) : base( message, exception) { }
+		public SellbriteNetworkException( string message ) : base( message ) { }
+	}
+
+	public class SellbriteUnauthorizedException : SellbriteException
+	{
+		public SellbriteUnauthorizedException( string message ) : base( message) { }
+	}
+
+	public class SellbriteRateLimitsExceeded : SellbriteNetworkException
+	{
+		public SellbriteRateLimitsExceeded( string message ) : base( message ) { }
+	}
+}
