7399a3b45224152e0a6da48866844b722bac841c
Maxim S <maxim.strelsov@skuvault.com>
2019-11-04 17:25:31 -0500

    Added SellbriteFactory > CreateProductsService

--- src/SellbriteAccess/ISellbriteFactory.cs
+++ src/SellbriteAccess/ISellbriteFactory.cs
@@ -1,10 +1,13 @@
 ﻿using SellbriteAccess.Configuration;
 using SellbriteAccess.Services.Orders;
+using SellbriteAccess.Services.Products;
 
 namespace SellbriteAccess
 {
 	public interface ISellbriteFactory
     {
 		ISellbriteOrdersService CreateOrdersService( SellbriteMerchantCredentials credentials );
+
+		ISellbriteProductsService CreateProductsService( SellbriteMerchantCredentials credentials );
     }
 }
